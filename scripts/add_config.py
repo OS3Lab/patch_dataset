3c0fb010d4bdeb120a4c58e538e2f00337f2750f
Song Jingyi <jingyisong@hust.edu.cn>
2024-06-05 17:37:43 +0800

    scripts: 自动生成 real.patch

--- scripts/add_config.py
+++ scripts/add_config.py
@@ -1,18 +1,49 @@
 # 输入：data.csv
 # CVE-2022-3964, ffmpeg, 92f9b28ed84a77138105475beba16c146bdaf984, ad28b01a141703b831256b712e0613281b15fcf0
 
-# 输出：部分填好的 cfg 文件, 包括 new_patch_parent, target_release
+# 输出：部分填好的 cfg 文件（包括 new_patch_parent, target_release），使用 git format-patch 生成的 real.patch 文件
 
 import os
 import requests
 from bs4 import BeautifulSoup
 
-url_map = {
+proj_url_map = {
     'ffmpeg': 'https://github.com/FFmpeg/FFmpeg'
 }
 
+# proj 与 patch_dataset 文件夹的相对路径
+proj_path_map = {
+    'ffmpeg': '../FFmpeg'
+}
 
-def write_cfg(project, project_url, cve_id, patch_release, parent_release, parent_backport):
+
+def write_patch(proj_name, cve_id, mannual_backport):
+    # 检查 project 和 cve 文件夹是否存在
+    if not os.path.exists(proj_name):
+        os.makedirs(proj_name)
+        print('create project folder: ', proj_name)
+    if not os.path.exists(os.path.join(proj_name, cve_id)):
+        os.makedirs(os.path.join(proj_name, cve_id))
+        print('create cve folder: ', os.path.join(proj_name, cve_id))
+
+    # 切换到 project 文件夹，生成 patch 文件
+    dataset_abs_path = os.getcwd()
+    proj_abs_path = os.path.abspath(proj_path_map[proj_name])
+    os.chdir(proj_abs_path)
+    os.system('git format-patch -1 %s -o %s' % (mannual_backport,
+              os.path.join(dataset_abs_path, proj_name, cve_id)))
+
+    # 切换到cve文件夹
+    os.chdir(os.path.join(dataset_abs_path, proj_name, cve_id))
+    os.system('mv *.patch real.patch')
+    print('generate patch file: ', os.path.join(
+        proj_name, cve_id, 'real.patch'))
+
+    # 返回patch_dataset文件夹
+    os.chdir(dataset_abs_path)
+
+
+def write_cfg(proj_name, proj_url, cve_id, patch_release, parent_release, parent_backport):
 
     # new_patch = patch_release_commit, 主线引入 patch 的 commit
     # new_patch_parent = new_patch 的 parent commit
@@ -25,28 +56,27 @@ target_release: %s
 sanitizer: 
 error_massage: 
 tag: %s'''
-    template = template % (project, project_url, patch_release,
+    template = template % (proj_name, proj_url, patch_release,
                            parent_release, parent_backport, cve_id)
-    
 
     # 检查 project 文件夹是否存在
-    if not os.path.exists(project):
-        os.makedirs(project)
-        print('create project folder: ', project)
+    if not os.path.exists(proj_name):
+        os.makedirs(proj_name)
+        print('create project folder: ', proj_name)
     else:
-        print('project folder exists: ', project)
+        print('project folder exists: ', proj_name)
 
     # 检查 cve 文件夹是否存在
-    if not os.path.exists(os.path.join(project, cve_id)):
-        os.makedirs(os.path.join(project, cve_id))
-        print('create cve folder: ', os.path.join(project, cve_id))
+    if not os.path.exists(os.path.join(proj_name, cve_id)):
+        os.makedirs(os.path.join(proj_name, cve_id))
+        print('create cve folder: ', os.path.join(proj_name, cve_id))
     else:
-        print('cve folder exists: ', os.path.join(project, cve_id))
+        print('cve folder exists: ', os.path.join(proj_name, cve_id))
 
-    with open(os.path.join(project, cve_id, 'config.yml'), 'w') as f:
+    with open(os.path.join(proj_name, cve_id, 'config.yml'), 'w') as f:
         f.write(template)
         print('write config file: ', os.path.join(
-            project, cve_id, 'config.yml'))
+            proj_name, cve_id, 'config.yml'))
 
 
 # 通过 commit url 获取 parent commit
@@ -66,7 +96,7 @@ def main():
         lines = f.readlines()
         for line in lines:
             cve_id, project, patch_release, mannual_backport = line.strip().split(',')
-            project_url = url_map[project]
+            project_url = proj_url_map[project]
             if 'https:' in patch_release:
                 patch_release = patch_release.split('/')[-1]
             if 'https:' in mannual_backport:
@@ -79,6 +109,7 @@ def main():
 
             write_cfg(project, project_url, cve_id, patch_release,
                       parent_release, parent_backport)
+            write_patch(project, cve_id, mannual_backport)
 
 
 if __name__ == '__main__':
@@ -89,6 +120,3 @@ if __name__ == '__main__':
         # 切换到 patch_dataset 文件夹
         os.chdir(os.path.dirname(os.getcwd()))
         main()
-    
-    
-
