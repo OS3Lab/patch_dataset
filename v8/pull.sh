bc6efc894f26d9619e921aa79348fa9c6e0aa7e4
Zhaoyang Li <lzy445850071@gmail.com>
2024-06-07 00:21:18 -0700

    libwebp: add patch case. v8: correct pull.sh

--- v8/pull.sh
+++ v8/pull.sh
@@ -1,4 +1,9 @@
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools
-export PATH="$PATH:`pwd`/depot_tools"
+if command -v gn &> /dev/null; then
+    echo " "
+else
+    if [ ! -d "depot_tools" ]; then
+        git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+    fi
+    export PATH="$PATH:$(pwd)/depot_tools"
+fi
 fetch v8
-rm -rf depot_tools
